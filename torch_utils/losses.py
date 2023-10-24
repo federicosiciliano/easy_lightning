f73fafd2bf40f53573d53f8132bda2c2b4a0b387
Federico Carmignani <82052662+fed21@users.noreply.github.com>
2023-10-24 18:13:24 +0200

    GCE classical

--- torch_utils/losses.py
+++ torch_utils/losses.py
@@ -4,38 +4,60 @@ import torch.nn.functional as F
 import math
 import numpy as np
 
-#https://github.com/AlanChou/Truncated-Loss
+#https://github.com/dmizr/phuber/blob/master/phuber/loss.py
 class GCELoss(nn.Module):
-
-    def __init__(self, q=0.7, k=0.5):
-        super(GCELoss, self).__init__()
+    """
+    Computes the Generalized Cross Entropy (GCE) loss, which is especially useful for 
+    training deep neural networks with noisy labels.
+    Refer to "Generalized Cross Entropy Loss for Training Deep Neural Networks with Noisy Labels"
+    <https://arxiv.org/abs/1805.07836>
+    
+    Attributes:
+        q (float): Box-Cox transformation parameter. Must be in (0,1].
+        epsilon (float): A small value to avoid undefined gradient.
+        softmax (nn.Softmax): Softmax function to convert raw scores to probabilities.
+    """
+    
+    def __init__(self, q: float = 0.7) -> None:
+        """
+        Initializes the GCELoss module.
+        
+        Args:
+            q (float): Box-Cox transformation parameter. Default is 0.7.
+        """
+        super().__init__()
         self.q = q
-        self.k = k
-        self.weights = {}
-             
-    def forward(self, logits, targets, indexes):
-        p = F.softmax(logits, dim=1)
-        Yg = torch.gather(p, 1, torch.unsqueeze(targets, 1))
+        self.epsilon = 1e-9  # A small value to avoid division by zero or log(0)
+        self.softmax = nn.Softmax(dim=1)  # Softmax function to get probabilities
+    
+    def forward(self, input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+        """
+        Compute the GCE loss between the predictions and targets.
         
-        # Initialize weights for new indexes
-        for index in indexes:
-            if index.item() not in self.weights:
-                self.weights[index.item()] = torch.nn.Parameter(data=torch.ones(1), requires_grad=False)
+        Args:
+            """
+            :param input: Predictions from the model (before softmax)
+                          shape: (batch_size, num_classes)
+            :param target: True labels (one-hot encoded)
+                           shape: (batch_size, num_classes)
+            """
+            
+        Returns:
+            torch.Tensor: The mean GCE loss.
+        """
+        # Apply softmax to the raw scores to get probabilities
+        p = self.softmax(input)
         
-        loss = ((1-(Yg**self.q))/self.q)*torch.stack([self.weights[i.item()] for i in indexes]) - \
-               ((1-(self.k**self.q))/self.q)*torch.stack([self.weights[i.item()] for i in indexes])
-        loss = torch.mean(loss)
-
-        return loss
-
-    def update_weight(self, logits, targets, indexes):
-        p = F.softmax(logits, dim=1)
-        Yg = torch.gather(p, 1, torch.unsqueeze(targets, 1))
-        Lq = ((1-(Yg**self.q))/self.q)
-        Lqk = ((1-(self.k**self.q))/self.q) * torch.ones_like(targets, dtype=torch.float)
-        Lqk = torch.unsqueeze(Lqk, 1)
+        # Multiply the softmax probabilities by the one-hot targets
+        # and sum across classes to get the correct class probability
+        p = torch.sum(p * target, dim=1)
         
-        condition = torch.gt(Lqk, Lq)
-        for i, index in enumerate(indexes):
-            self.weights[index.item()] = condition[i].float().to(logits.device)
+        # Add epsilon to avoid undefined gradient due to log(0) or division by zero
+        p += self.epsilon
+        
+        # Compute the GCE loss based on the selected probability and the Box-Cox transformation parameter
+        loss = (1 - p ** self.q) / self.q
+        
+        # Return the mean loss
+        return torch.mean(loss)
 
