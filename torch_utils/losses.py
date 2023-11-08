e3285499edd817ca55f4d2cc1c162776b4a723c4
Federico Carmignani <82052662+fed21@users.noreply.github.com>
2023-11-09 00:41:43 +0100

    Update losses.py

--- torch_utils/losses.py
+++ torch_utils/losses.py
@@ -44,7 +44,7 @@ class BackwardNRL(nn.Module):
      
     def forward(self, input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
         log_probs = F.log_softmax(input, dim=1)
-        a = torch.matmul(self.matrix_inv, -log_probs.t()).t()
+        a = torch.matmul(self.matrix_inv, log_probs.t()).t()
         loss = torch.sum(a * target, dim=1) #minus here or above???
         return torch.mean(loss)
 
