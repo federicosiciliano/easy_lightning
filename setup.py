65efe121956c4a868c25096e59729557d8df5a92
Federico Siciliano <77665712+siciliano-diag@users.noreply.github.com>
2024-03-20 23:22:53 +0100

    Prova (#1)
    * delete data_utils
    
    * prova1
    
    * prova2
    
    * prova3
    
    * prova4
    
    * prova5
    
    * prova6
    
    * prova7
    
    * prova8
    
    * prova9
    
    * prova10
    
    * exp
    
    * exp1
    
    * torch1
    
    * rec_utils
    
    * submodules updated
    
    * submodules updated

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 # Read the contents of the README.md file for the long description
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-# Initialize an empty list for the installation requirements
-install_requires = []
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
 
 # Check if a requirements.txt file exists and if so, read its contents
 if os.path.isfile("requirements.txt"):
@@ -16,13 +16,16 @@ if os.path.isfile("requirements.txt"):
 # Define the package setup configuration
 setup(
     name='Easy Lightning',  # Replace with your package name
-    packages=['exp_utils', 'data_utils', 'torch_utils'],  # List of all packages included in your project
+    packages = find_packages(),  # List of all packages included in your project
     description='Easy Lightning: Simplify AI-Deep learning with PyTorch Lightning',  
     long_description=long_description,  # Use the contents of README.md as the long description
     long_description_content_type="text/markdown",
     version='0.0.1',  # Specify the version of your package
-    install_requires=install_requires,  # List of required dependencies
-    url='https://github.com/fed21',  # Replace with the URL of your GitHub repository
+    install_requires=install_requires+['data_utils @ git+https://github.com/siciliano-diag/data_utils.git',
+                                       'exp_utils @ git+https://github.com/siciliano-diag/exp_utils.git',
+                                       'rec_utils @ git+https://github.com/siciliano-diag/rec_utils.git',
+                                       'torch_utils @ git+https://github.com/siciliano-diag/torch_utils.git'],  # List of required dependencies
+    url='https://github.com/siciliano-diag/easy_lightning.git',  # Replace with the URL of your GitHub repository
     author='Federico Siciliano, Federico Carmignani',
     author_email='siciliano@diag.uniroma1.it, carmignanifederico@gmail.com',
     keywords=['DeepLearning', 'MachineLearning', 'PyTorch', 'Lightning', 'AI']  # Keywords related to your package
--- torch_utils/_version.py
+++
@@ -1,2 +0,0 @@
-# Define the version number for the package.
-__version__ = "0.0.1"
