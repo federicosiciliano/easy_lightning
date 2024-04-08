4bc45101000989bda9616e58cc1d98bf5fb8e668
Federico Siciliano <77665712+federicosiciliano@users.noreply.github.com>
2024-04-08 09:13:42 +0200

    Release (#2)
    * Initial commit
    
    * Update README.md
    
    * integration exp_utils
    
    integration exp_utils uncommented
    
    * Integration data_utils
    
    Integration data_utils uncommented
    
    * integration torch_utils
    
    integration torch_utils uncommented
    
    * resnet18+minst_ntb
    
    notebook for ResNet18 and MNIST dataset
    
    * commenting code partially
    
    commenting code partially
    
    * exp_utils commented
    
    exp_utils commented
    
    * exp_utils and data_utils commented
    
    exp_utils and data_utils commented
    
    * comments, readme and information_ntb
    
    comments, readme and information_ntb
    
    * update
    
    updating requirement.txt
    
    * Update requirements.txt
    
    * Update torch.py
    
    * custom losses handling
    
    * GCE loss in custom losses
    
    * Rename losses to losses.py
    
    * Update losses.py
    
    * Update losses.py
    
    * Update losses.py
    
    * Update losses.py
    
    * Update process.py
    
    * GCE classical
    
    * Update losses.py
    
    * Update losses.py
    
    * Update losses.py
    
    * Noise Robust Loss
    
    * Update losses.py
    
    * Update losses.py
    
    * Update losses.py
    
    * Update losses.py
    
    * Update losses.py
    
    * Update losses.py
    
    * Update losses.py
    
    * Update losses.py
    
    * Update losses.py
    
    * Update losses.py
    
    * Update losses.py
    
    * Update losses.py
    
    * Update losses.py
    
    * Update losses.py
    
    * Update losses.py
    
    * Update losses.py
    
    * Update losses.py
    
    * Update losses.py
    
    * Update losses.py
    
    * Update losses.py
    
    * Update losses.py
    
    * Update losses.py
    
    * Update losses.py
    
    * Update losses.py
    
    * Update losses.py
    
    * Update model.py
    
    * Update model.py
    
    * Update model.py
    
    * Update model.py
    
    * Update model.py
    
    * NCODLoss manual optimization
    
    * Update model.py
    
    * Update preparation.py
    
    * Update model.py
    
    * Update model.py
    
    * NCODLoss encodings
    
    * Update losses.py
    
    * Update preparation.py
    
    * Update preparation.py
    
    * Update losses.py
    
    * Update losses.py
    
    * Update losses.py
    
    * Update losses.py
    
    * Update model.py
    
    * Update model.py
    
    * Update model.py
    
    * Update model.py
    
    * LR scheduler and optimization of NCODLoss as in the paper
    
    * Update model.py
    
    * Update model.py
    
    * Update model.py
    
    * Update model.py
    
    * Update model.py
    
    * NCODLoss
    
    * Update model.py
    
    * Update preparation.py
    
    * Update model.py
    
    * Update model.py
    
    * Update model.py
    
    * Update model.py
    
    * Update model.py
    
    * Update model.py
    
    * Update losses.py
    
    * Update losses.py
    
    * Update losses.py
    
    * Update losses.py
    
    * Update model.py
    
    * Update model.py
    
    * Update model.py
    
    * Update model.py
    
    * Update model.py
    
    * Update model.py
    
    * Update model.py
    
    * Update model.py
    
    * Update model.py
    
    * Update model.py
    
    * Update model.py
    
    * Update model.py
    
    * Update model.py
    
    * Update model.py
    
    * Update model.py
    
    * Update model.py
    
    * Update model.py
    
    * Update model.py
    
    * Update model.py
    
    * Update losses.py
    
    * Update model.py
    
    * Update model.py
    
    * Prova (#1)
    
    * delete data_utils
    
    * prova1
    
    * prova2
    
    * prova3
    
    * prova4
    
    * prova5
    
    * prova6
    
    * prova7
    
    * prova8
    
    * prova9
    
    * prova10
    
    * exp
    
    * exp1
    
    * torch1
    
    * rec_utils
    
    * submodules updated
    
    * submodules updated
    
    * version1
    
    * submodule branch
    
    * solve sequential
    
    * new gitignore
    
    * new gitignore
    
    * changed name from utils to easy
    
    * submodule links
    
    * updated gitignore and submodules
    
    * fix install metadata?
    
    * fix rec_torch_utils
    
    * easy_rec update + base notebook
    
    * change git username
    
    * release change account
    
    * update username submodules
    
    * Update model.py
    
    * rebase?
    
    * release
    
    * relase3
    
    ---------
    
    Co-authored-by: Federico Carmignani <82052662+fed21@users.noreply.github.com>
    Co-authored-by: Federico Siciliano <77665712+siciliano-diag@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -20,12 +20,12 @@ setup(
     description='Easy Lightning: Simplify AI-Deep learning with PyTorch Lightning',  
     long_description=long_description,  # Use the contents of README.md as the long description
     long_description_content_type="text/markdown",
-    version='0.0.1',  # Specify the version of your package
-    install_requires=install_requires+['data_utils @ git+https://github.com/siciliano-diag/data_utils.git',
-                                       'exp_utils @ git+https://github.com/siciliano-diag/exp_utils.git',
-                                       'rec_utils @ git+https://github.com/siciliano-diag/rec_utils.git',
-                                       'torch_utils @ git+https://github.com/siciliano-diag/torch_utils.git'],  # List of required dependencies
-    url='https://github.com/siciliano-diag/easy_lightning.git',  # Replace with the URL of your GitHub repository
+    version='1.0.0',  # Specify the version of your package
+    install_requires=install_requires+['easy_data @ git+https://github.com/federicosiciliano/easy_data.git',
+                                       'easy_exp @ git+https://github.com/federicosiciliano/easy_exp.git',
+                                       'easy_rec @ git+https://github.com/federicosiciliano/easy_rec.git',
+                                       'easy_torch @ git+https://github.com/federicosiciliano/easy_torch.git'],  # List of required dependencies
+    url='https://github.com/federicosiciliano/easy_lightning.git',  # Replace with the URL of your GitHub repository
     author='Federico Siciliano, Federico Carmignani',
     author_email='siciliano@diag.uniroma1.it, carmignanifederico@gmail.com',
     keywords=['DeepLearning', 'MachineLearning', 'PyTorch', 'Lightning', 'AI']  # Keywords related to your package
