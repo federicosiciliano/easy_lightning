7bb4bc368674579ad852a40af38a4cb98cf336c4
Filippo Betello <betello@diag.uniroma1.it>
2025-07-19 15:47:27 +0200

    quickstartrec finalized

--- ntb/quick_start_rec.py
+++ ntb/quick_start_rec.py
@@ -1,92 +1,65 @@
 #Put all imports here
-import numpy as np
 import os
-import sys
 import easy_lightning
-from easy_lightning import easy_exp, easy_rec, easy_torch
+import sys
+from copy import deepcopy
 
-print("easy_rec path:", easy_rec.__path__)
+project_root = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.join(project_root, '../src'))
 
-
-
-#every path should start from the project folder:
-project_folder = "../"
-
-#Config folder should contain hyperparameters configurations
-cfg_folder = os.path.join(project_folder,"cfg/easy_rec_cfg")
-
-#Data folder should contain raw and preprocessed data
-data_folder = os.path.join(project_folder,"data")
-raw_data_folder = os.path.join(data_folder,"raw")
+from easy_lightning2 import easy_exp, easy_rec, easy_torch
 
 
-# data loading
-cfg = easy_exp.cfg.load_configuration("config_rec", config_path=cfg_folder)
-cfg["data_params"]["data_folder"] = raw_data_folder
+def main():
+    #every path should start from the project folder:
+    project_folder = "../"
 
-# Example of a sweep configuration
-for _ in cfg.sweep(cfg["data_params"]["dataset_params"]["lookback"]):
+    #Config folder should contain hyperparameters configurations
+    cfg_folder = os.path.join(project_folder,"cfg/easy_rec_cfg")
 
-    data, maps = easy_rec.data_generation_utils.preprocess_dataset(**cfg["data_params"])
-    datasets = easy_rec.rec_torch.prepare_rec_datasets(data,**cfg["data_params"]["dataset_params"])
-
-
-    # model loading
-    cfg["model"]["loader_params"]["num_items"] = np.max(list(maps["sid"].values()))
-    loaders = easy_rec.rec_torch.prepare_rec_data_loaders(datasets, data, **cfg["model"]["loader_params"])
-    cfg["model"]["rec_model"]["num_items"] = np.max(list(maps["sid"].values()))
-    cfg["model"]["rec_model"]["num_users"] = np.max(list(maps["uid"].values()))
-    cfg["model"]["rec_model"]["lookback"] = cfg["data_params"]["dataset_params"]["lookback"]
-    main_module = easy_rec.rec_torch.create_rec_model(**cfg["model"]["rec_model"])
-
-    # check exp
-    exp_found, experiment_id = easy_exp.exp.get_set_experiment_id(cfg)
-    print("Experiment already found:", exp_found, "----> The experiment id is:", experiment_id)
+    #Data folder should contain raw and preprocessed data
+    data_folder = os.path.join(project_folder,"data")
+    raw_data_folder = os.path.join(data_folder,"raw")
 
 
-    if exp_found: exit() #TODO: make the notebook/script stop here if the experiment is already found
+    # cfg loading
+    cfg = easy_exp.cfg.load_configuration("config_rec", config_path=cfg_folder)
+    cfg["data_params"]["data_folder"] = raw_data_folder
 
 
-    trainer_params = easy_torch.preparation.prepare_experiment_id(cfg["model"]["trainer_params"], experiment_id)
+    # Example of a sweep configuration
+    for _ in cfg.sweep('data_params.collator_params.lookback'):
 
-    # Prepare callbacks and logger using the prepared trainer_params
-    trainer_params["callbacks"] = easy_torch.preparation.prepare_callbacks(trainer_params)
-    trainer_params["logger"] = easy_torch.preparation.prepare_logger(trainer_params)
-
-    # Prepare the trainer using the prepared trainer_params
-    trainer = easy_torch.preparation.prepare_trainer(**trainer_params)
-
-    model_params = cfg["model"].copy()
-
-    model_params["loss"] = easy_torch.preparation.prepare_loss(cfg["model"]["loss"], easy_rec.losses)
-
-    # Prepare the optimizer using configuration from cfg
-    model_params["optimizer"] = easy_torch.preparation.prepare_optimizer(**cfg["model"]["optimizer"])
-
-    # Prepare the metrics using configuration from cfg
-    model_params["metrics"] = easy_torch.preparation.prepare_metrics(cfg["model"]["metrics"], easy_rec.metrics)
-
-    # Create the model using main_module, loss, and optimizer
-    model = easy_torch.process.create_model(main_module, **model_params)
+        # check exp
+        exp_found, experiment_id = easy_exp.exp.get_set_experiment_id(cfg)
+        print("Experiment already found:", exp_found, "----> The experiment id is:", experiment_id)
+        if exp_found: continue
 
 
-    # Prepare the emission tracker using configuration from cfg
-    tracker = easy_torch.preparation.prepare_emission_tracker(**cfg["model"]["emission_tracker"], experiment_id=experiment_id)
+        data, maps = easy_rec.preparation.prepare_rec_data(cfg)
+        collator_params = deepcopy(cfg["data_params"]["collator_params"])
+        loaders = easy_rec.preparation.prepare_rec_dataloaders(cfg, data, maps, collator_params=collator_params)
 
+        # model loading
+        main_module = easy_rec.preparation.prepare_rec_model(cfg, maps)
 
-    # Train the model using the prepared trainer, model, and data loaders
-    easy_torch.process.train_model(trainer, model, loaders, tracker=tracker, val_key=["val","test"])
+        # Prepare the trainer using the prepared trainer_params
+        trainer = easy_torch.preparation.complete_prepare_trainer(cfg, experiment_id, additional_module=easy_rec)#, raytune=raytune)
 
+        model = easy_torch.preparation.complete_prepare_model(cfg, main_module, easy_rec)
+                        
+        # tracker = easy_torch.preparation.prepare_emission_tracker(**cfg["model"]["emission_tracker"], experiment_id=experiment_id)
+        
+        # profiler =  easy_torch.preparation.prepare_flops_profiler(model, **cfg["model"]["flops_profiler"], experiment_id=experiment_id)
 
-    easy_torch.process.test_model(trainer, model, loaders, tracker=tracker)
-
-
-    # Save experiment and print the current configuration
-    #save_experiment_and_print_config(cfg)
-    easy_exp.exp.save_experiment(cfg)
-
-    # Print completion message
-    print("Execution completed.")
-    print("######################################################################")
-    print()
-
+                
+        easy_torch.process.train_model(trainer, model, loaders, val_key=["val","test"])
+        
+        easy_torch.process.test_model(trainer, model, loaders, test_key=["val","test","train"]) #, tracker=tracker,  profiler=profiler)
+        
+        # Save experiment
+        easy_exp.exp.save_experiment(cfg)
+        
+if __name__ == "__main__":
+    main()
+    print("Quick start for recommendation finished successfully!")
